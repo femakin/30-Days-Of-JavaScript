e552dc4ecec9ed1d992afca533577509366f833f
Femi Akinyemi <santosfemakin@yahoo.com>
2021-03-12 15:52:46 +0100

    Completed day_one task

---
+++ 01_Day_Introduction/Day_one_solution/datatypes.js
@@ -0,0 +1,15 @@
+// Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+// Declaring different variables of different data types
+let firstName = 'Asabeneh' // first name of a person
+let lastName = 'Yetayeh' // last name of a person
+let country = 'Finland' // country
+let city = 'Helsinki' // capital city
+let age = 100 // age in years
+let isMarried = true
+
+// Checking Datatype of the declared variables
+
+console.log(typeof(firstName));
+console.log(typeof(isMarried));
+console.log(typeof(city));
--- 01_Day_Introduction/variable.js
+++ 01_Day_Introduction/variable.js
@@ -15,3 +15,30 @@ const PI = 3.14 // geometrical constant
 let name = 'Asabeneh', //name of a person
   job = 'teacher',
   live = 'Finland'
+
+  // Declare four variables without assigning values
+
+  let amount, school, president, region
+
+  // Declare four variables with assigned values
+
+  let company = "Google";
+  let number_of_staff = 5000;
+  let role = "software Engineer"
+  let Salary = 700000
+
+  // Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+  let firstname_ = "Femi";
+  let lastname_ = "Akinyemi";
+  let maritalstatus_ = "Married";
+  let Country_ = "Nigeria";
+  let age_ = 18
+
+  // Declare variables to store your first name, last name, marital status, country and age in a single line
+
+  let [firstname__, lastname__, maritalstatus__, country__, age__  ] = ["Femi", "Akinyemi", "Married", "Nigeria", 18]
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+let [myage, yourage] = [18, 20];
+console.log(myage)
